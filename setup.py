3df1a67b43bcdcd0467255520f9e2f3683235b5c
Amit Schendel <amitschendel@gmail.com>
2024-11-15 14:41:15 +0200

    Bumping version
    Signed-off-by: Amit Schendel <amitschendel@gmail.com>

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="langsec",
-    version="0.1.0-alpha",
+    version="0.3.0-alpha",
     packages=find_packages(where="src"),
     package_dir={"": "src"},
     install_requires=[
